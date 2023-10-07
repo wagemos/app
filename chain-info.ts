8c530c23ff6b487e07bf28ea37280c1ec21a2721
Josef Leventon <josef.leventon@gmail.com>
2023-10-07 14:22:32 +0300

    Update rpc

--- chain-info.ts
+++ chain-info.ts
@@ -2,7 +2,7 @@ export const CHAIN_INFO = [
   {
     chain_name: 'neutron',
     chain_id: 'neutron-1',
-    rpc: 'https://neutron-rpc.polkachu.com:443',
-    api: 'https://neutron-api.polkachu.com:443',
+    rpc: 'https://rpc-kralum.neutron-1.neutron.org:443',
+    api: 'https://rest-kralum.neutron-1.neutron.org/',
   },
 ]
